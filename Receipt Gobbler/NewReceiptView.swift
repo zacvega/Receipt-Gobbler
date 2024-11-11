86ef5dd7e326c10c138f98a4ad62557d86fac956
Zac Vega <zacvega@hotmail.com>
2024-11-10 20:50:25 -0600

    Added new view

--- Receipt Gobbler/NewReceiptView.swift
+++ Receipt Gobbler/NewReceiptView.swift
@@ -4,38 +4,15 @@ struct NewReceiptView: View {
     @State private var showCamera = false
     @State private var showCameraAlert = false
     @State private var capturedImage: UIImage?
-    
+    @State private var recognizedText = "Tap button to start scanning"
     //for navigation
     @State private var isButtonPressed: Bool = false
 
     var body: some View {
         VStack {
             // Take a Picture Button
-            Button(action: {
-                if UIImagePickerController.isSourceTypeAvailable(.camera) {
-                    showCamera = true
-                } else {
-                    showCameraAlert = true
-                }
-            }) {
-                Text("Scan")
-                    .font(.title)
-                    .padding()
-                    .frame(width: 200.0)
-                    .background(.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(30)
-            }
-            .padding()
-            .sheet(isPresented: $showCamera) {
-                CameraView(image: $capturedImage)
-            }
-            .alert(isPresented: $showCameraAlert) {
-                Alert(title: Text("Camera Not Available"), message: Text("Your device does not support camera functionality."), dismissButton: .default(Text("OK")))
-            }
             
-            
-            
+        
             NavigationLink{ ReceiptFormView()
             } label: {
                 Text("Type")
@@ -46,6 +23,23 @@ struct NewReceiptView: View {
                     .foregroundStyle(.white)
                     .cornerRadius(30)
             }
+            NavigationLink{ ScanDocumentView(recognizedText: $recognizedText)
+            } label: {
+                Text("Scan")
+                    .font(.title)
+                    .padding()
+                    .frame(width: 200.0)
+                    .background(.cyan)
+                    .foregroundStyle(.white)
+                    .cornerRadius(30)
+            }
+            Text("\(recognizedText)")
+                            .padding()
+                            .frame(width: 300)
+                            .background(Color.blue.opacity(0.1))
+                            .cornerRadius(8)
+                            .multilineTextAlignment(.center)
+            
 
 //            // Type In Button
 //            NavigationLink(destination: ReceiptFormView()) {
